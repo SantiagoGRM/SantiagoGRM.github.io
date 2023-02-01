82803bfbf1f8d2571e3cd684652188f6acc7fb7e
SantiagoGRM <santi-971@live.com.ar>
2023-02-01 18:39:53 -0300

    mi primer repositorio

---
+++ javascript/function.js
@@ -0,0 +1,17 @@
+
+// Definimos la funcion hola mundo
+function holaMundo () {
+    console.log("Gracias por visitar mi curriculum!");
+}
+
+//Invocamos la funcion holaMundo
+holaMundo();
+
+function sumar(a,b) {
+    var resultado = a+b;
+
+    return a+b;
+}
+
+suma = sumar(5,5);
+console.log(suma);
